08c2c84d5f7298082e03212cab8a2ba71767f545
Ty Raino <mstymetris@yahoo.com>
2020-03-15 23:25:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,6 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |holiday, items|
   puts "#{holiday}: #{items}"
 end
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
