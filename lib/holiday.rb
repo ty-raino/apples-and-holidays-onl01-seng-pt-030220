d4dde562215a6442617530a1adf185f8b1db6b83
Ty Raino <mstymetris@yahoo.com>
2020-03-15 23:25:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |holiday, items|
+  puts "#{holiday}: #{items}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
