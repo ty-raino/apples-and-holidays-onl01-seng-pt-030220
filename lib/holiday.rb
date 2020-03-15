5384b6e34b5fed8f7fe2c740e437acdcd4dda3c3
Ty Raino <mstymetris@yahoo.com>
2020-03-15 23:37:28 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
+  holiday_supplies[:summer][:fourth_of_july][1]
+
   # given that holiday_hash looks like this:
   # {
   #   :winter => {
@@ -24,25 +26,30 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+holiday_hash[:winter].each do |holiday, items|
+items << supply
+ end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
+  holiday_hash[season][holiday_name] = supply_array
   # remember to return the updated hash
-
+holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+array = holiday_hash[:winter].map do |holidays, items|
+  items
+end
+array.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,18 +60,25 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |season, holiday|
+  puts season.to_s.capitalize! << ":"
+  holiday.each do |holiday, supplies|
+    supplies = supplies.join(", ")
+    holiday = holiday.to_s.split("_").map {|w| w.capitalize}.join(" ") << ":"
+    puts "  " + holiday + " " + supplies
+  end
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+holiday_hash.map do |season, holidays|
+  holidays.map do |holiday, items|
+    if items.include?("BBQ")
+      holiday
+    end
+  end
 end
-
-
-
-
-
-
-
+.flatten.compact
+end
